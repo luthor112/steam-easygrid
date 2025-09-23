cd95be07316a9db5502cb12c7fec1548eed78a3d
luthor112 <luthor112@gmail.com>
2025-09-23 19:03:54 +0200

    Update libs, bump version, add back some removed safeguards

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -299,6 +299,13 @@ export default async function PluginMain() {
     console.log("[steam-easygrid 3] frontend startup");
     await App.WaitForServicesInitialized();
 
+    while (
+        typeof g_PopupManager === 'undefined' ||
+        typeof MainWindowBrowserManager === 'undefined'
+    ) {
+        await sleep(100);
+    }
+
     const doc = g_PopupManager.GetExistingPopup("SP Desktop_uid0");
 	if (doc) {
 		OnPopupCreation(doc);
